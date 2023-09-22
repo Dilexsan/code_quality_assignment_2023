fe4ae92c2564104332fce8a562848e888dfb712e
Dilexsan <120619547+Dilexsan@users.noreply.github.com>
2023-09-22 11:08:25 +0000

    changes2

--- travel_cost_calculator.py
+++ travel_cost_calculator.py
@@ -1,49 +1,53 @@
+
+# Dilexsan H.A.L EG/2020/3896
+
 from csv import *
 
 a = {}
-b = {}
+currency_array = {}
 c = {}
 
-def lhr(file):  
+def hotel_rates(file):  
     with open(file) as h:
         r = reader(h)
         for row in r:
             a[row[0]] = float(row[1])
 
-def ler(file): 
+def exchange_rates(file): 
     with open(file) as e:
         r = reader(e)
         for row in r:
-            b[row[0].upper()] = float(row[1]) * 1 
+            currency_array[row[0].upper()] = float(row[1]) * 1 
 
-def lfr(file):
+def flight_costs(file):
     with open(file) as f:
         r = reader(f)
         for row in r:
             c[row[0]] = float(row[1])
 
 def main():
-    lhr('data/hotel_rates.csv')
-    ler('data/exchange_rates.csv')
-    lfr('data/flight_costs.csv')
+    
+    hotel_rates('data/hotel_rates.csv')
+    exchange_rates('data/exchange_rates.csv')
+    flight_costs('data/flight_costs.csv')
 
-    d = input("Enter your destination: ").upper()
+    destination = input("Enter your destination: ").upper()
 
-    f = c.get(d, 0.0)
-    h = a.get(d, 0.0)
+    f = c.get(destination, 0.0)
+    hours = a.get(destination, 0.0)
 
     days = int(input("Enter your stay duration in days: "))
-    h *= days
-    total = f + h
+    hours *= days
+    total = f + hours
 
     print(f"Flight cost: USD {f:.2f}")
-    print(f"Hotel cost for {days} days: USD {h:.2f}")
+    print(f"Hotel cost for {days} days: USD {hours:.2f}")
     print(f"Total: USD {total:.2f}")
 
-    currency = input(f"Select your currency for final price estimation ({', '.join(b.keys())}): ")
+    currency = input(f"Select your currency for final price estimation ({', '.join(currency_array.keys())}): ")
 
-    p = total * b[currency]
-    print(f"Total in {currency}: {p:.2f}")
+    price = total * currency_array[currency]
+    print(f"Total in {currency}: {price:.2f}")
 
 if __name__ == "__main__":
     main()
